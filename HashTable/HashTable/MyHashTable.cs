6c95bbc18d5868a3987f875910e00c87a716a5f5
Nicholas Page <npage@southhills.edu>
2025-10-05 18:17:46 -0400

    Initial commit

---
+++ HashTable/HashTable/MyHashTable.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Name: [YOUR NAME HERE]
+ * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+ */
+// Remember to put your name and SH username above
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable;
+
+public class MyHashTable<TKey, TValue>
+    : IEnumerable<DictionaryEntry>
+{
+    // Using an unsigned integer here to mimic std::size_t
+    private const uint DEFAULT_CAPACITY = 16;
+    private List<KeyValuePair<TKey, TValue>>[] buckets;
+
+    public int Count { get; private set; } = 0; // <- Should increase when adding a new key-value pair
+    public int Buckets => buckets.Length; // <- Should return the number of buckets
+    
+    public IEnumerable<TKey> Keys
+    {
+        get
+        {
+            // TODO: Implement this method
+            return [];
+        }
+    }
+    
+    public IEnumerable<TValue> Values
+    {
+        get
+        {
+            // TODO: Implement this method
+            return [];
+        }
+    }
+
+    public TValue this[TKey key]
+    {
+        get
+        {
+            // TODO: Implement this method
+            return default;
+        }
+    }
+
+    // TODO: Implement a default constructor
+
+    // TODO: Implement a constructor that takes a capacity
+
+    /// <summary>
+    /// Computes the bucket index for the specified key.
+    /// </summary>
+    /// <typeparam name="TKey">
+    /// The type of the key used in the hash table.
+    /// </typeparam>
+    /// <param name="key">
+    /// The key to compute a hash index for.
+    /// </param>
+    /// <returns>
+    /// A non-negative index within the range of the buckets array.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="key"/> is <c>null</c>.
+    /// </exception>
+    /// <remarks>
+    /// This method obtains the hash code of the provided key and then ensures it maps
+    /// to a valid bucket index. The cast to <see cref="uint"/> guarantees that even
+    /// if the hash code is negative (including <see cref="int.MinValue"/>), the
+    /// resulting value will be non-negative before taking the modulo. This avoids
+    /// issues with <see cref="Math.Abs(int)"/> where <see cref="int.MinValue"/> cannot
+    /// be represented as a positive <see cref="int"/>.
+    /// </remarks>
+    private int GetHashIndex(TKey key)
+    {
+        if (key == null) throw new ArgumentNullException(nameof(key));
+
+        int hashCode = key.GetHashCode();
+        return (int)((uint)hashCode % (uint)buckets.Length);
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(TKey key)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public override String ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+       
+        // TODO: Implement this method
+        // This method should use StringBuilder rather than string concatenation
+        //  to avoid creating unnecessary garbage in the heap.
+        
+        return sb.ToString();
+    }
+
+    public void Clear()
+    {
+        buckets = new List<KeyValuePair<TKey, TValue>>[buckets.Length];
+        // GC will collect the old buckets
+        Count = 0;
+    }
+    
+    #region IEnumerable implementation
+    public IEnumerator<DictionaryEntry> GetEnumerator()
+    {
+        foreach (List<KeyValuePair<TKey, TValue>> bucket in buckets)
+        {
+            foreach (KeyValuePair<TKey, TValue> kvp in bucket)
+            {
+                yield return new DictionaryEntry(kvp.Key!, kvp.Value!);
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    #endregion
+}
+
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Running;
+
+namespace HashTable;
+
+class Program
+{
+    public static void Main()
+    {
+#if DEBUG
+        TestMyHashTable();
+#else
+		BenchmarkRunner.Run<Benchmark>();
+#endif
+    }
+    
+    private static void TestMyHashTable()
+    { 
+        MyHashTable<String, float> menu = new MyHashTable<String, float>();
+        menu.Add("Pizza", 9.99f);
+        menu.Add("Burger", 5.99f);
+        menu.Add("Fries", 2.99f);
+        menu.Add("Soda", 1.99f);
+        menu.Add("Salad", 4.99f);
+        menu.Add("Steak", 12.99f);
+
+        Console.WriteLine(menu);
+        Console.WriteLine("Count: " + menu.Count);
+        Debug.Assert(menu.Count == 6);
+        Console.WriteLine("Buckets: " + menu.Buckets);
+        Debug.Assert(menu.Buckets >= 16);
+
+        Console.WriteLine("Press enter to continue...");
+        Console.ReadLine();
+
+        foreach(String key in menu.Keys)
+        {
+            Console.WriteLine($"Price of {key}: ${menu[key]}");
+        }
+    }
+}
